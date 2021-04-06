b192e4d905dcf36e6ea0eec4fdcc2c3068579433
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-04-07 09:40:13 +1000

    Добавил пример c Singleton(одиночка)

---
+++ Singletone/SingletoneTut/SingletonTut/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, varibles contain different instances.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
