fdc64bf81eae31f0878aff2218f1e80567516a37
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-04-07 10:05:09 +1000

    Добавил пример потоко безопасного Singletone(одиночки)

---
+++ Singletone/SingletoneTut/SingletoneMultiTheadExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace SingletoneMultiTheadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+            
+            Console.ReadLine();
+        }
+
+        public static void TestSingleton(string value)
+        {
+            SingletoneThreadSafe singleton = SingletoneThreadSafe.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        }
+    }
+}
---
+++ Singletone/SingletoneTut/SingletoneMultiTheadExample/SingletoneThreadSafe.cs
@@ -0,0 +1,32 @@
+﻿namespace SingletoneMultiTheadExample
+{
+    // Версия одиночки для нескольких потоков
+    public class SingletoneThreadSafe
+    {
+        private SingletoneThreadSafe() { }
+
+        private static SingletoneThreadSafe _instance;
+
+        // объект-блокировка для синхронизации потоков
+        private static readonly object _lock = new object();
+        
+        public static SingletoneThreadSafe GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletoneThreadSafe();
+                        _instance.Value = value;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+        
+        public string Value { get; set; }
+    }
+}
